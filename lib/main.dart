98c25c8ccc8d725800bcf67221ae595bb755882a
Sachin Kumar <ojaz07860@gmail.com>
2022-11-29 18:42:34 +0530

    Added Firebase to Application.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
-import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'login/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -11,11 +18,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const GetMaterialApp(
       title: 'Attendance App',
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
     );
   }
 }
-
